62102eef363c2340f57f7c1169194105b1992cbb
aryan-ra44 <163700022+aryan-ra44@users.noreply.github.com>
2024-09-17 23:30:59 -0400

    Update mario.c

--- mario.c
+++ mario.c
@@ -1,29 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main (void)
+int main(void)
 
 {
-    int height, row, space, hashtag;
+    int height;
 
     do
     {
-        height = get_int ("Height: ");
+        height = get_int("Height: ");
     }
-    while (height<1);
+    while (height < 1);
 
-    for (row = 0, row<height; row = row + 1)
+    for (int row = 0; row < height; row++)
     {
-        for (space = 0; space<=row; space = space + 1)
+        for (int space = 0; space < height - row - 1; space++)
         {
-            printf(" ")
+            printf(" ");
         }
-        for (hashtag = 0; hashtag<=row; hashtag = hashtag + 1)
+        for (int hashtag = 0; hashtag <= row; hashtag++)
         {
             printf("#");
         }
-
+        printf("\n");
     }
-
-
 }
