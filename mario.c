8604eb8b0c43e2b5f08085f5ef8b302a15543659
aryan-ra44 <163700022+aryan-ra44@users.noreply.github.com>
2024-10-01 21:43:39 -0400

    Update mario.c

--- mario.c
+++ mario.c
@@ -1,17 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
-
 int main(void)
 
 {
     int height;
-
     do
     {
         height = get_int("Height: ");
     }
     while (height < 1);
-
     for (int row = 0; row < height; row++)
     {
         for (int space = 0; space < height - row - 1; space++)
